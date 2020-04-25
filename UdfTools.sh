3ed3e7fb2d987a2ee83c7c5738da091309b751fa
Unidentified Sec Team <64120626+Unidentified-tm@users.noreply.github.com>
2020-04-25 18:51:39 +0430

    Create UdfTools.sh

---
+++ UdfTools.sh
@@ -0,0 +1,527 @@
+#!/usr/bin/bash
+#Coded By j4riko
+#Any Copying and Editing is forbidden
+clear
+sleep 2
+echo ""
+echo "\033[34;1m               ________________________________________________________"
+sleep 0
+echo "\033[35;1m                               in the name of ALLAH "
+sleep 0
+echo "\033[32;1m                                  Coded By J4riko "
+sleep 0 
+echo "\033[37;1m                         Telegram CHannel=> @Unidentified_tm"
+sleep 0
+echo "\033[31;1m                         TEAM => UNIDENTIFIED Security TEAM "
+sleep 0
+echo "\033[36;1m                                   {Uni Tools} "
+sleep 0
+echo "\033[33;1m                      The Best Channel For Learning Hack And Security"
+sleep 0
+echo "\033[33;1m   Pwk-CEH-Network Security-Exploit-Scripts All and All in CHannel UniDentiFied_TM"
+sleep 0
+echo "\033[36;1m                          The Best Hackers Private Team !!"
+sleep 0
+echo "\033[36;1m               ________________________________________________________"
+echo ""
+echo "                          1. Red_Hawk (Information Gathering) "
+echo "                          2. EvilURL (Information Gathering) " 
+echo "                          3. Nmap (Information Gathering) "
+echo "                          4. Automater (Information Gathering) "
+echo "                          5. ReconDog (Information Gathering) "
+echo "                          6. Wpscan (Information Gathering) "
+echo "                          7. sslyze (Information Gathering) "
+echo "                          8. Trackout (Information Gathering) "
+echo "                          9. Recon-ng (Information Gathering) "
+echo "                         10. Masscan (Information Gathering) "
+echo "                         11. Nikto (Information Gathering) "
+echo "                         12. Smbmap (Information Gathering) "
+echo "                         13. CMSmap (Information Gathering) "
+echo "                         14. D-TECT (Information Gathering) "
+echo "                         15. Wpscan (Information Gathering) "
+echo "                         16. Devploit (Information Gathering) "
+echo "                         17. Sublist3r (Information Gathering) "
+echo "                         18. inSPY (Information Gathering) "
+echo "                         19. Crips (Information Gathering) "
+echo "                         20. DnsRecon (Information Gathering) "
+echo "                         21. Hydra (Password Attacker) "
+echo "                         22. CeWL (Password attacker)  "
+echo "                         23. Hash-Buster (Password Attacker) "
+echo "                         24. JohnTheRipper (Password Attacker  "
+echo "                         25. SET (Passwod Attacker)  "
+echo "                         26. SecLists (Password Attacker)  "
+echo "                         27. Socialfish (Password Attacker[fishing])"
+echo "                         28. Creddump (Password Attacker) "
+echo "                         29. CrowBar (Password Attacker) "
+echo "                         30. Cupp (Password Attacker) "
+echo "                         31. FindmyHash (Password Attacker) "
+echo "                         32. Hash Generator (Password Attacker) "
+echo "                         33. Hashcat (Password Attacker)          "
+echo "                         34. Hasherdotid (Password Attacker) "
+echo "                         35. Hasher (Password Attacker) "
+echo "                         36. Hydra (Password Attacker) "
+echo "                         37. Johnny (Password Attacker) "
+echo "                         38. Keimpx (Password Attacker) "
+echo "                         39. Maskprocessor (Password Attacker) "
+echo "                         40. Patator (Password Attacker) "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                                               "
+echo "\033[32;1m"
+read -p "=> " J4riko
+
+if [ $J4riko = 1 ] || [ $J4riko = 1 ]
+then
+clear
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+exit
+fi
+
+if [ $J4riko = 2 ] || [ $J4riko = 2 ]
+then
+clear
+git clone https://github.com/UndeadSec/EvilURL 
+exit
+fi
+
+if [ $J4riko = 3 ] || [ $J4riko = 3 ]
+then
+clear
+apt-get install nmap
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone https://github.com/1aN0rmus/TekDefense-Automater
+exit
+fi
+
+if [ $J4riko = 5 ] || [ $J4riko = 5 ]
+then
+clear
+git clone https://github.com/s0md3v/ReconDog
+exit
+fi
+
+if [ $J4riko = 6 ] || [ $J4riko = 6 ]
+then
+clear
+git clone https://github.com/04x/WpscaN
+exit
+fi
+
+if [ $J4riko = 7 ] || [ $J4riko = 7 ]
+then
+clear
+git clone https://github.com/nabla-c0d3/sslyze
+exit
+fi
+
+if [ $J4riko = 8 ] || [ $J4riko = 8 ]
+then
+clear
+git clone https://github.com/abaykan/TrackOut
+exit
+fi
+
+
+if [ $J4riko = 9 ] || [ $J4riko = 9 ]
+then
+clear
+git clone https://github.com/lanmaster53/recon-ng
+exit
+fi
+
+if [ $J4riko = 10 ] || [ $J4riko = 10 ]
+then
+clear
+git clone https://github.com/robertdavidgraham/masscan
+exit
+fi
+
+if [ $J4riko = 11 ] || [ $J4riko = 11 ]
+then
+clear
+git clone https://github.com/sullo/nikto
+exit
+fi
+
+if [ $J4riko = 12 ] || [ $J4riko = 12 ]
+then
+clear
+git clone https://github.com/ShawnDEvans/smbmap
+exit
+fi
+
+if [ $J4riko = 13 ] || [ $J4riko = 13 ]
+then
+clear
+git clone https://github.com/Dionach/CMSmap
+exit
+fi
+
+if [ $J4riko = 14 ] || [ $J4riko = 14 ]
+then
+clear
+git clone https://github.com/shawarkhanethicalhacker/D-TECT-1
+exit
+fi
+
+if [ $J4riko = 15 ] || [ $J4riko = 15 ]
+then
+clear
+git clone https://github.com/wpscanteam/wpscan
+exit
+fi
+
+if [ $J4riko = 16 ] || [ $J4riko = 16 ]
+then
+clear
+git clone https://github.com/GhettoCole/Devploit
+exit
+fi
+
+if [ $J4riko = 17 ] || [ $J4riko = 17 ]
+then
+clear
+git clone https://github.com/aboul3la/Sublist3r
+exit
+fi
+
+if [ $J4riko = 18 ] || [ $J4riko = 18 ]
+then
+clear
+git clone https://github.com/leapsecurity/InSpy
+exit
+fi
+
+if [ $J4riko = 19 ] || [ $J4riko = 19 ]
+then
+clear
+git clone https://github.com/Manisso/Crips
+exit
+fi
+
+if [ $J4riko = 20 ] || [ $J4riko = 20 ]
+then
+clear
+git clone https://github.com/darkoperator/dnsrecon
+exit
+fi
+
+if [ $J4riko = 21 ] || [ $J4riko = 21 ]
+then
+clear
+apt-get install hydra -y
+exit
+fi
+
+if [ $J4riko = 22 ] || [ $J4riko = 22 ]
+then
+clear
+git clone https://github.com/digininja/CeWL
+exit
+fi
+
+if [ $J4riko = 23 ] || [ $J4riko = 23 ]
+then
+clear
+git clone https://github.com/s0md3v/Hash-Buster
+exit
+fi
+
+if [ $J4riko = 24 ] || [ $J4riko = 24 ]
+then
+clear
+git clone https://github.com/magnumripper/JohnTheRipper
+exit
+fi
+
+if [ $J4riko = 25 ] || [ $J4riko = 25 ]
+then
+clear
+git clone https://github.com/trustedsec/social-engineer-toolkit
+exit
+fi
+
+if [ $J4riko = 26 ] || [ $J4riko = 26 ]
+then
+clear
+git clone https://github.com/danielmiessler/SecLists
+exit
+fi
+
+if [ $J4riko = 27 ] || [ $J4riko = 27 ]
+then
+clear
+git clone https://github.com/UndeadSec/SocialFish
+exit
+fi
+
+if [ $J4riko = 28 ] || [ $J4riko = 28 ]
+then
+clear
+git clone https://github.com/moyix/creddump
+exit
+fi
+
+if [ $J4riko = 29 ] || [ $J4riko = 29 ]
+then
+clear
+git clone https://github.com/galkan/crowbar
+exit
+fi
+
+if [ $J4riko = 30 ] || [ $J4riko = 30 ]
+then
+clear
+git clone https://github.com/Mebus/cupp
+exit
+fi
+
+if [ $J4riko = 31 ] || [ $J4riko = 31 ]
+then
+clear
+git clone https://github.com/frdmn/findmyhash
+exit
+fi
+
+if [ $J4riko = 32 ] || [ $J4riko = 32 ]
+then
+clear
+git clone https://github.com/CiKu370/hash-generator
+exit
+fi
+
+if [ $J4riko = 33 ] || [ $J4riko = 33 ]
+then
+clear
+git clone
+echo "sorry !"
+exit
+fi
+
+if [ $J4riko = 34 ] || [ $J4riko = 34 ]
+then
+clear
+git clone https://github.com/galauerscrew/hasherdotid
+exit
+fi
+
+if [ $J4riko = 35 ] || [ $J4riko = 35 ]
+then
+clear
+git clone https://github.com/CiKu370/hasher
+exit
+fi
+
+if [ $J4riko = 36 || [ $J4riko = 36 ]
+then
+clear
+git clone 
+exit
+fi
+
+if [ $J4riko = 37 ] || [ $J4riko = 37 ]
+then
+clear
+git clone https://github.com/shinnok/johnny
+exit
+fi
+
+if [ $J4riko = 38 ] || [ $J4riko = 38 ]
+then
+clear
+git clone https://github.com/nccgroup/keimpx
+exit
+fi
+
+if [ $J4riko = 39 ] || [ $J4riko = 39 ]
+then
+clear
+git clone https://github.com/hashcat/maskprocessor
+exit
+fi
+
+if [ $J4riko = 40 ] || [ $J4riko = 40 ]
+then
+clear
+git clone https://github.com/lanjelot/patator
+exit
+fi
+
+if [ $J4riko = 41 ] || [ $J4riko = 41 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 42 ] || [ $J4riko = 42 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 43 ] || [ $J4riko = 43 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 44 ] || [ $J4riko = 44 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 45 ] || [ $J4riko = 45 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 46 ] || [ $J4riko = 46 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 47 ] || [ $J4riko = 47 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 48 ] || [ $J4riko = 48 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 49 ] || [ $J4riko = 49 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 50 ] || [ $J4riko = 50 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+if [ $J4riko = 4 ] || [ $J4riko = 4 ]
+then
+clear
+git clone
+exit
+fi
+
+
+
